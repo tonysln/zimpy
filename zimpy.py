76480dbf581b19130cb8ef6ca3beeae7c135ca2c
pierresquarra <pierre@squarra.de>
2023-10-03 16:00:50 +0200

    cleaned, removed db batches

--- zimpy.py
+++ zimpy.py
@@ -1,6 +1,5 @@
 import mmap
 import sqlite3
-from typing import List, Tuple
 
 from flask import Flask, Response, render_template, request
 from tqdm import trange
@@ -9,14 +8,14 @@ from structs import *
 
 
 def bisect(compare_function, low: int, high: int):
-    """Bisect the given range using the given compare function"""
+    """Bisect a sorted list and return the index of the element that matches the compare function"""
     while low < high:
         middle = (low + high) // 2
         comp = compare_function(middle)
         if comp == 0:
             return middle
         if comp < 0:
-            low = middle
+            low = middle + 1
         else:
             high = middle
 
@@ -25,6 +24,7 @@ def bisect(compare_function, low: int, high: int):
 
 class ZIMFile:
     def __init__(self, file_path: str) -> None:
+        """Open the ZIM file and read the header"""
         with open(file_path, "rb") as f:
             self._mm = mmap.mmap(f.fileno(), 0, access=mmap.ACCESS_READ)
             self.header = Header(self._mm, 0)
@@ -35,7 +35,7 @@ class ZIMFile:
             print(self.header)
 
     def _compare_url(self, index: int, ns: bytes, url: str) -> int:
-        """Compare the url of the dirent at the given index with the given url in the given namespace"""
+        """Compare the url at the given index with the given url"""
         d = Dirent(self.header.buf, self.urlPtrList[index])
         if d.namespace == ns and d.url == url:
             return 0
@@ -45,12 +45,15 @@ class ZIMFile:
             return 1
 
     def findByUrl(self, ns: bytes, url: str) -> int:
-        """Find the index of the dirent with the given url in the given namespace"""
-        return bisect(lambda index: self._compare_url(index, ns, url), 0, self.header.entryCount)
+        """Find the index of the url in the urlPtrList"""
+        try:
+            return bisect(lambda index: self._compare_url(index, ns, url), 0, self.header.entryCount)
+        except IndexError:
+            return -1
 
 
-def _initialize_db(zim: ZIMFile, batch_size: int = 1000) -> None:
-    """Create and populate the database with the entries from the zim file"""
+def _initialize_db(zim: ZIMFile) -> None:
+    """Initialize the database and populate it with the articles"""
     with sqlite3.connect("wiki.db") as conn:
         c = conn.cursor()
 
@@ -68,21 +71,14 @@ def _initialize_db(zim: ZIMFile, batch_size: int = 1000) -> None:
             return
 
         print("Populating database...")
-        _article_count = 0
-        _batch: List[Tuple[str, str, str]] = []
         for i in trange(zim.header.entryCount):
             _dirent = Dirent(zim.header.buf, zim.urlPtrList[i])
             if _dirent.namespace == b"A":
-                _article_count += 1
                 _title = _dirent.title or _dirent.url
-                _batch.append((_title, _dirent.url, _dirent.namespace.decode("utf-8")))
+                _entry = (_title, _dirent.url, _dirent.namespace.decode("utf-8"))
+                c.executemany("INSERT INTO entries (title, url, namespace) VALUES (?, ?, ?)", (_entry,))
 
-                if len(_batch) == batch_size:
-                    c.executemany("INSERT INTO entries (title, url, namespace) VALUES (?, ?, ?)", _batch)
-                    _batch.clear()
-
-        if _batch:
-            c.executemany("INSERT INTO entries (title, url, namespace) VALUES (?, ?, ?)", _batch)
+        _article_count = c.execute("SELECT COUNT(*) FROM entries").fetchone()[0]
         print(f"Found {_article_count} articles, creating indexes...")
         c.execute("CREATE INDEX IF NOT EXISTS title_index ON entries (title)")
         conn.commit()
@@ -90,6 +86,7 @@ def _initialize_db(zim: ZIMFile, batch_size: int = 1000) -> None:
 
 class ZIMServer:
     def __init__(self, file_path: str, template: str) -> None:
+        """Initialize the ZIM server"""
         self._zim = ZIMFile(file_path)
         self._template = template
         self.app = Flask(__name__)
@@ -106,17 +103,17 @@ class ZIMServer:
 
         @self.app.route("/favicon.ico")
         def favicon():
-            """No favicon"""
+            """Return a 204 No Content response for the favicon"""
             return "No content", 204
 
         @self.app.route("/w/<path:url>")
         def w(url):
-            """The pages always request /w/load.php... but it's not found in the zim file"""
+            """A /w/load.php... is requested, return a 204 No Content response"""
             return f"No content for {url[:5]}", 204
 
         @self.app.route("/search")
         def search():
-            """Search for the given query"""
+            """Search for a query in the database"""
             _query = request.args.get("q")
             if not _query:
                 return "No query", 400
@@ -126,17 +123,26 @@ class ZIMServer:
                 db_query = "%" + "%".join(_query.split()) + "%"
                 c.execute("SELECT title, url FROM entries WHERE title LIKE ? ORDER BY LENGTH(title) LIMIT 100",
                           (db_query,))
-                results = c.fetchall()
+                _results = c.fetchall()
 
-            return render_template("search.html", query=_query, results=results)
+            if not _results:
+                return render_template("base.html", head="", body=f"No results for {_query}")
+
+            if len(_results) == 1:
+                return url(_results[0][1])
+
+            return render_template("search.html", query=_query, results=_results)
 
         @self.app.route("/<path:url>")
         def url(url):
-            """Render the given url or return the content if it's not html"""
+            """Return the requested url"""
             if "/" not in url:
                 url = "A/" + url
             _ns, _url = url.split("/", 1)
-            _dirent = Dirent(self._zim.header.buf, self._zim.urlPtrList[self._zim.findByUrl(bytes(_ns, "utf-8"), _url)])
+            _idx = self._zim.findByUrl(bytes(_ns, "utf-8"), _url)
+            if _idx == -1:
+                return render_template("base.html", head="", body=f"Page {url} not found")
+            _dirent = Dirent(self._zim.header.buf, self._zim.urlPtrList[_idx])
             while _dirent.kind == "redirect":
                 _index = _dirent.redirect_index
                 _dirent = Dirent(self._zim.header.buf, self._zim.urlPtrList[_index])
@@ -149,12 +155,12 @@ class ZIMServer:
             return _response
 
     def _render_template(self, content: str) -> str:
-        """Render the given content as a template"""
+        """Render the template with the given content"""
         _head = content.split("<head>", 1)[1].split("</head>", 1)[0]
         _body = content.split("<body", 1)[1].split(">", 1)[1].rsplit("</body>", 1)[0]
         return render_template(self._template, head=_head, body=_body)
 
 
 if __name__ == '__main__':
-    server = ZIMServer("wiki.zim", template="base.html")
-    server.app.run(port=5000)
+    server = ZIMServer("wiki.zim", "base.html")
+    server.app.run()
