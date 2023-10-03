9e743cdfb91f21248dbf2cc251d7cfe113a3a342
pierresquarra <pierre@squarra.de>
2023-10-03 14:17:55 +0200

    final tweaks, added progress bar

--- zimpy.py
+++ zimpy.py
@@ -1,15 +1,14 @@
-import sqlite3
 import mmap
+import sqlite3
 from typing import List, Tuple
 
 from flask import Flask, Response, render_template, request
+from tqdm import trange
+
 from structs import *
 
-ARTICLE = "A"
-OTHER = "-"
 
-
-def bisect(compare_function, low, high):
+def bisect(compare_function, low: int, high: int):
     """Bisect the given range using the given compare function"""
     while low < high:
         middle = (low + high) // 2
@@ -25,14 +24,14 @@ def bisect(compare_function, low, high):
 
 
 class ZIMFile:
-    def __init__(self, file_path: str):
+    def __init__(self, file_path: str) -> None:
         with open(file_path, "rb") as f:
-            self.mm = mmap.mmap(f.fileno(), 0, access=mmap.ACCESS_READ)
-            self.header = Header(self.mm, 0)
-            self.mimeList = MimeTypeList(self.mm, self.header.mimeListPos)
-            self.urlPtrList = UrlPtrList(self.mm, self.header.urlPtrPos)
-            self.titlePtrList = TitlePtrList(self.mm, self.header.titlePtrPos)
-            self.clusterPtrList = ClusterPtrList(self.mm, self.header.clusterPtrPos)
+            self._mm = mmap.mmap(f.fileno(), 0, access=mmap.ACCESS_READ)
+            self.header = Header(self._mm, 0)
+            self.mimeList = MimeTypeList(self._mm, self.header.mimeListPos)
+            self.urlPtrList = UrlPtrList(self._mm, self.header.urlPtrPos)
+            self.titlePtrList = TitlePtrList(self._mm, self.header.titlePtrPos)
+            self.clusterPtrList = ClusterPtrList(self._mm, self.header.clusterPtrPos)
             print(self.header)
 
     def _compare_url(self, index: int, ns: bytes, url: str) -> int:
@@ -45,28 +44,12 @@ class ZIMFile:
         else:
             return 1
 
-    def _compare_title(self, index: int, ns: bytes, title: str) -> int:
-        """Compare the title of the dirent at the given index with the given title in the given namespace"""
-        urlIndex = self.titlePtrList[index]
-        d = Dirent(self.header.buf, self.urlPtrList[urlIndex])
-        title_from_data = d.title or d.url
-        if d.namespace == ns and title_from_data == title:
-            return 0
-        if d.namespace < ns or (d.namespace == ns and title_from_data < title):
-            return -1
-        else:
-            return 1
-
-    def findByUrl(self, ns, url) -> int:
+    def findByUrl(self, ns: bytes, url: str) -> int:
         """Find the index of the dirent with the given url in the given namespace"""
         return bisect(lambda index: self._compare_url(index, ns, url), 0, self.header.entryCount)
 
-    def findByTitle(self, ns, title) -> int:
-        """Find the index of the dirent with the given title in the given namespace"""
-        return bisect(lambda index: self._compare_title(index, ns, title), 0, self.header.entryCount)
 
-
-def create_and_populate_db(zim: ZIMFile, batch_size: int = 1000) -> None:
+def _initialize_db(zim: ZIMFile, batch_size: int = 1000) -> None:
     """Create and populate the database with the entries from the zim file"""
     with sqlite3.connect("wiki.db") as conn:
         c = conn.cursor()
@@ -85,94 +68,93 @@ def create_and_populate_db(zim: ZIMFile, batch_size: int = 1000) -> None:
             return
 
         print("Populating database...")
-        batch: List[Tuple[str, str, str]] = []
-        for i in range(zim.header.entryCount):
-            if i % (zim.header.entryCount // 10) == 0:
-                print(f"{i / zim.header.entryCount * 100:.2f}%")
-            dirent = Dirent(zim.header.buf, zim.urlPtrList[i])
-            if dirent.namespace.decode("utf-8") == ARTICLE:
-                title = dirent.title or dirent.url
-                batch.append((title, dirent.url, dirent.namespace.decode("utf-8")))
+        _article_count = 0
+        _batch: List[Tuple[str, str, str]] = []
+        for i in trange(zim.header.entryCount):
+            _dirent = Dirent(zim.header.buf, zim.urlPtrList[i])
+            if _dirent.namespace == b"A":
+                _article_count += 1
+                _title = _dirent.title or _dirent.url
+                _batch.append((_title, _dirent.url, _dirent.namespace.decode("utf-8")))
 
-                if len(batch) == batch_size:
-                    c.executemany("INSERT INTO entries (title, url, namespace) VALUES (?, ?, ?)", batch)
-                    batch.clear()
+                if len(_batch) == batch_size:
+                    c.executemany("INSERT INTO entries (title, url, namespace) VALUES (?, ?, ?)", _batch)
+                    _batch.clear()
 
-        if batch:
-            c.executemany("INSERT INTO entries (title, url, namespace) VALUES (?, ?, ?)", batch)
+        if _batch:
+            c.executemany("INSERT INTO entries (title, url, namespace) VALUES (?, ?, ?)", _batch)
+        print(f"Found {_article_count} articles, creating indexes...")
         c.execute("CREATE INDEX IF NOT EXISTS title_index ON entries (title)")
         conn.commit()
-        print("Done")
-
-
-def rank_results(query: str, results: list) -> list:
-    """Rank the results by the length of the match divided by the length of the title"""
-    def ranker(item):
-        title, url = item
-        match_length = len(query)
-        title_length = len(title)
-        score = match_length / title_length
-        return -score
-
-    return sorted(results, key=ranker)
 
 
 class ZIMServer:
-    def __init__(self, file_path: str):
-        self.zim = ZIMFile(file_path)
+    def __init__(self, file_path: str, template: str) -> None:
+        self._zim = ZIMFile(file_path)
+        self._template = template
         self.app = Flask(__name__)
 
-        create_and_populate_db(self.zim)
+        _initialize_db(self._zim)
 
         @self.app.route("/")
         def index():
-            dirent = Dirent(self.zim.header.buf, self.zim.urlPtrList[self.zim.header.mainPage])
-            cluster = Cluster(self.zim.header.buf, self.zim.clusterPtrList[dirent.clusterNumber])
-            return cluster.get_blob_data(dirent.blobNumber)
+            """Render the main page"""
+            _dirent = Dirent(self._zim.header.buf, self._zim.urlPtrList[self._zim.header.mainPage])
+            _cluster = Cluster(self._zim.header.buf, self._zim.clusterPtrList[_dirent.clusterNumber])
+            _content = _cluster.get_blob_data(_dirent.blobNumber).decode("utf-8")
+            return self._render_template(_content)
 
         @self.app.route("/favicon.ico")
         def favicon():
+            """No favicon"""
             return "No content", 204
 
         @self.app.route("/w/<path:url>")
         def w(url):
-            return "Not found", 404
+            """The pages always request /w/load.php... but it's not found in the zim file"""
+            return f"No content for {url[:5]}", 204
 
         @self.app.route("/search")
         def search():
-            query = request.args.get("q")
-            if not query:
+            """Search for the given query"""
+            _query = request.args.get("q")
+            if not _query:
                 return "No query", 400
 
             with sqlite3.connect("wiki.db") as conn:
                 c = conn.cursor()
-                db_query = "%" + "%".join(query.split()) + "%"
-                c.execute("SELECT title, url FROM entries WHERE title LIKE ? LIMIT 1000", (db_query,))
+                db_query = "%" + "%".join(_query.split()) + "%"
+                c.execute("SELECT title, url FROM entries WHERE title LIKE ? ORDER BY LENGTH(title) LIMIT 100",
+                          (db_query,))
                 results = c.fetchall()
 
-            results = rank_results(query, results)
-            return render_template("search.html", query=query, results=results)
+            return render_template("search.html", query=_query, results=results)
 
         @self.app.route("/<path:url>")
         def url(url):
+            """Render the given url or return the content if it's not html"""
             if "/" not in url:
-                url = str(ARTICLE) + "/" + url
+                url = "A/" + url
             _ns, _url = url.split("/", 1)
-            dirent = Dirent(self.zim.header.buf, self.zim.urlPtrList[self.zim.findByUrl(bytes(_ns, "utf-8"), _url)])
-            while dirent.kind == "redirect":
-                index = dirent.redirect_index
-                dirent = Dirent(self.zim.header.buf, self.zim.urlPtrList[index])
-            cluster = Cluster(self.zim.header.buf, self.zim.clusterPtrList[dirent.clusterNumber])
-            content = cluster.get_blob_data(dirent.blobNumber)
-            if self.zim.mimeList[dirent.mimetype] == "text/html":
-                head = content.split(b"<head>", 1)[1].split(b"</head>", 1)[0]
-                body = content.split(b"<body", 1)[1].split(b">", 1)[1].rsplit(b"</body>", 1)[0]
-                return render_template("base.html", head=head.decode("utf-8"), body=body.decode("utf-8"))
-            response = Response(content)
-            response.headers['Content-Type'] = self.zim.mimeList[dirent.mimetype]
-            return response
+            _dirent = Dirent(self._zim.header.buf, self._zim.urlPtrList[self._zim.findByUrl(bytes(_ns, "utf-8"), _url)])
+            while _dirent.kind == "redirect":
+                _index = _dirent.redirect_index
+                _dirent = Dirent(self._zim.header.buf, self._zim.urlPtrList[_index])
+            _cluster = Cluster(self._zim.header.buf, self._zim.clusterPtrList[_dirent.clusterNumber])
+            _content = _cluster.get_blob_data(_dirent.blobNumber)
+            if self._zim.mimeList[_dirent.mimetype] == "text/html":
+                return self._render_template(_content.decode("utf-8"))
+            _response = Response(_content)
+            _response.headers['Content-Type'] = self._zim.mimeList[_dirent.mimetype]
+            return _response
+
+    def _render_template(self, content: str) -> str:
+        """Render the given content as a template"""
+        _head = content.split("<head>", 1)[1].split("</head>", 1)[0]
+        _body = content.split("<body", 1)[1].split(">", 1)[1].rsplit("</body>", 1)[0]
+        return render_template(self._template, head=_head, body=_body)
 
 
 if __name__ == '__main__':
-    server = ZIMServer("wiki.zim")
-    server.app.run()
+    server = ZIMServer("wiki.zim", template="base.html")
+    server.app.run(port=5000)
