e91d21ea264132d04ba09e900d2fc073cc803691
Anton Slavin <slavinant@gmail.com>
2025-05-16 00:36:36 +0300

    List zim files and allow choice

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
 from zimpy import WikiServer
+import pathlib
+import glob
 
-WikiServer("wiki.zim")
+
+w = list(pathlib.Path("/Users/tony/zim/").glob("*.zim"))
+print('The following ZIM files were found:')
+for i in range(len(w)):
+    print(f' {i+1}) {w[i].name.replace(".zim", "")}')
+
+print('')
+idx = int(input('> '))-1
+
+assert idx >= 0 and idx < len(w)
+WikiServer(w[idx])
